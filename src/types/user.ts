d26e0837110475eb5caf94de4725f0935ab478b4
Nazar Koval <n.kovaldev@gmail.com>
2023-06-06 15:54:56 +0300

    set initial types
    * for local user
    * for remote user
    * for unknown user

---
+++ src/types/user.ts
@@ -0,0 +1,36 @@
+export interface ILocalUser {
+	login: string
+	id: number
+	avatar: string
+	name: string
+	company: string
+	blog: string
+	location: string
+	bio: string
+	twitter: string
+	repos: number
+	followers: number
+	following: number
+	created: string
+}
+
+export interface IUser {
+	login: string
+	id: number
+	avatar_url: string
+	name: string
+	company: string
+	blog: string
+	location: string
+	bio: string
+	twitter_username: string
+	public_repos: number
+	followers: number
+	following: number
+	created_at: string
+}
+
+export interface IGithubError {
+	message: string
+	documentation_url: string
+}
