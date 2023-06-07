230b637453206a2c062b2f530853f0acd3876a8c
Nazar Koval <n.kovaldev@gmail.com>
2023-06-07 21:04:10 +0300

    create mock data and fix types possible options

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { IUser } from "../types"
+
+export const defaultUser: IUser = {
+	login: "Liknox",
+	id: 92331274,
+	avatar_url: "https://avatars.githubusercontent.com/u/92331274?v=4",
+	name: "Nazar Koval",
+	company: null,
+	blog: "",
+	location: "Lviv, Ukraine",
+	bio: "It's no problem in my local.",
+	twitter_username: null,
+	public_repos: 17,
+	followers: 4,
+	following: 3,
+	created_at: "2021-10-11T18:10:04Z",
+}
--- src/types/user.ts
+++ src/types/user.ts
@@ -19,11 +19,11 @@ export interface IUser {
 	id: number
 	avatar_url: string
 	name: string
-	company: string
-	blog: string
-	location: string
-	bio: string
-	twitter_username: string
+	company: string | null
+	blog: string | null
+	location: string | null
+	bio: string | null
+	twitter_username: string | null
 	public_repos: number
 	followers: number
 	following: number
