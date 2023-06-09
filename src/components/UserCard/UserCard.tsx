83b05568e5e4d2de738cb260b1cc956b1b69eff9
Nazar Koval <n.kovaldev@gmail.com>
2023-06-09 15:57:38 +0300

    add target="_blank" to link and format files

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -13,7 +13,7 @@ export const UserCard = (props: UserCardPropsType) => {
 			<img src={props.avatar} alt={props.name} className={styles.avatar} />
 			<UserTitle name={props.name} login={props.login} created={props.created} url={props.url} />
 			<p className={`${styles.bio}${props.bio ? "" : ` ${styles.empty}`}`}>{props.bio || "This profile has no bio."}</p>
-			<UserStats repos={props.repos} followers={props.followers} following={props.following} name={props.login}  />
+			<UserStats repos={props.repos} followers={props.followers} following={props.following} name={props.login} />
 			<UserInfo blog={props.blog} company={props.company} location={props.location} twitter={props.twitter} />
 		</div>
 	)
--- src/components/UserStatsInfo/UserStatsInfo.tsx
+++ src/components/UserStatsInfo/UserStatsInfo.tsx
@@ -7,11 +7,13 @@ interface UserStatsInfoProps extends Pick<ILocalUser, "name"> {
 }
 
 export const UserStatsInfo = ({ name, section, quantity }: UserStatsInfoProps) => {
-   const reqSection = section.toLowerCase()
+	const reqSection = section.toLowerCase()
 	return (
 		<div className={styles.info}>
 			<div className={styles.infoTitle}>
-				<a href={`https://github.com/${name}?tab=${reqSection === "repos" ? "repositories" : reqSection}`}>{section}</a>
+				<a href={`https://github.com/${name}?tab=${reqSection === "repos" ? "repositories" : reqSection}`} target="_blank" rel="noreferrer">
+					{section}
+				</a>
 			</div>
 			<div className={styles.infoNumber}>{quantity}</div>
 		</div>
